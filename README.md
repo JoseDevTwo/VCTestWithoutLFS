7be7edb9e6f34fba23c915b90dd1f14c08225f31
Jose <ajose9977@gmail.com>
2023-12-18 21:19:58 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# VCTestWithoutLFS
+
+Developed with Unreal Engine 5
---
+++ Source/VCTestWithoutLFS/VCTestWithoutLFSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VCTestWithoutLFSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AVCTestWithoutLFSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AVCTestWithoutLFSGameMode();
+};
+
+
+
